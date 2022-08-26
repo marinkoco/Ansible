79fb0f4151c4127169b8ea06b71fe68871fa5ebd
marinkoco <69796741+marinkoco@users.noreply.github.com>
2022-08-26 18:17:07 +0200

    Add files via upload

---
+++ CRON_TEST/fileSize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo -n "[INFO]" ; echo -n $current_time ; echo -n ": " ; du -sh ~/Documents/CRON_TEST/cronFile.txt
+
+#du -sh /home/marinkoco/Documents/CRON_TEST/cronFile.txt
+
